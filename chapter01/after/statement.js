84d9e1ee96af98ebfb55ff89e61821d442b6ff7b
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-02-04 18:41:59 +0900

    チャプター１　リファクタリングサンプル

--- chapter01/after/statement.js
+++ chapter01/after/statement.js
@@ -1,94 +1,49 @@
 import plays from './plays.json';
 import invoice from './invoices.json';
+import createStatementData from './createStatementData.js'
+
+export {statement}
+export {htmlStatement}
 
 console.log(statement(invoice[0], plays));
 
-function statement (invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${ invoice.customer }\n`;
-        
-    for (let perf of invoice.performances) {
-        //注文の内訳を出力
-        result += ` ${ playFor(perf).name }: ${ usd(amountFor(pref)) } (${ perf.audience } seats)\n`;
-        totalAmount += amountFor(pref);
-    }
-
-    result += `Amount owed is ${ usd(totalAmount) }\n`;
-    result += `You earned ${ totalVolumeCredits() } credits\n`;
-    return result;
-}
-
-/**
- * 
- */
-function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of invoice.performances) {
-        result += volumeCreditsFor(pref)
-    }
-    return result;
-
-}
-
-/**
- * 
- * @param {*} aNumber 
- */
 function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
         {
             style: "currency", currency: "USD",
             minimumFractionDigits: 2
-        }).format(aNumber/100);
+        }).format(aNumber / 100);
 }
 
-/**
- * 
- * @param {*} aPerformance 
- */
-function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    //ボリューム特典のポイントを加算
-    result += Math.max(aPerformance.audience - 30, 0);
-    //喜劇のときは10人につき、さらにポイントを加算
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-}
-
-/**
- * 
- * @param {*} aPerformance 
- */
-function playFor (aPerformance) {
-    return plays[aPerformance.playID];
-}
-
-/**
- * 
- * @param {*} aPerformance 
- */
-function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${ playFor(aPerformance).type }`);
+function renderPlainText(statementData) {
+    let result = `Statement for ${statementData.customer}\n`;
+    for (let perf of statementData.performances) {
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
     }
 
+    result += `Amount owed is ${usd(statementData.totalAmount)}\n`;
+    result += `You earned ${statementData.totalVolumeCredits} credits\n`;
     return result;
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
 }

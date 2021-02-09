13b367ca104d2d105758fa34e98bd451a7306b07
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-02-09 18:26:35 +0900

    関数のインライン化

---
+++ chapter06/GetRating.ts
@@ -0,0 +1,20 @@
+// 関数のインライン化
+
+function beforeGetRating(driver) {
+    return moreThanFiveLateDeliveries(driver) ? 2 : 1
+}
+
+function moreThanFiveLateDeliveries(driver) {
+    return driver.numberOfLateDeliveries > 5
+}
+
+
+
+
+/**
+ * ===================================================
+ */
+
+ function getRating(driver) {
+     return (driver.numberOfLateDeliveries > 5) ? 2 : 1
+ }

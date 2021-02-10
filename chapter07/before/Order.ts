a8d623a07c13f715e4a1e167688b243596c629cf
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-02-10 21:47:10 +0900

    オブジェクトによるプリミティブの置き換え

---
+++ chapter07/before/Order.ts
@@ -0,0 +1,28 @@
+class Priority {
+    [x: string]: any;
+
+    constructor(value) {
+        if (Priority.legalValues().includes(value))
+            this._value = value;
+        else
+            throw new Error(`<${value}> is invalid for Priority`);
+    }
+    toString() {return this._value;}
+    get _index() {return Priority.legalValues().findIndex(s => s === this._value);}
+    static legalValues() {return ['low', 'normal', 'high', 'rush'];}
+
+    equals(other) {return this._index === other._index;}
+    higherThan(other) {return this._index > other._index;}
+    lowerThan(other) {return this._index < other._index;}
+}
+
+export class Order {
+    [x: string]: any;
+    constructor(data) {
+        this._priority = new Priority(data.priority);
+    }
+}
+
+const orders = [new Order({priority: "normal"}), new Order({priority: "high"}), new Order({priority: "rush"})];
+
+export const highPriorityCount = orders.filter(o => o.priority.higherThan(new Priority("normal"))).length;

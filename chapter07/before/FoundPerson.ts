c9137f2b35806dc4ce5b37cb1100eb98e2287a6e
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-02-12 16:13:15 +0900

    アルゴリズムの置き換え

---
+++ chapter07/before/FoundPerson.ts
@@ -0,0 +1,15 @@
+// アルゴリズムの置き換え
+function foundPerson(people) {
+    for (let i = 0; i < people.length; i++) {
+        if(people[i] === "Don") {
+            return "Don"
+        }
+        if(people[i] === "John") {
+            return "John"
+        }
+        if(people[i] === "Kent") {
+            return "Kent"
+        }
+    }
+    return ''
+}
